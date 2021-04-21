2e4c4a94ca6ca6f345ae8e4fe3f6e4ef16f2666f
Patrick Wolfe <wolfep15@gmail.com>
2021-04-21 13:07:24 -0400

    Add minifb

---
+++ src/gb.rs
@@ -0,0 +1,6 @@
+pub use self::dimensions::PIXELS_X as screen_width;
+pub use self::dimensions::PIXELS_Y as screen_height;
+pub mod dimensions {
+    pub const PIXELS_X: usize = 144;
+    pub const PIXELS_Y: usize = 160;
+}
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate minifb;
+use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
+
+mod gb;
+
+fn main() {
+    let mut buffer: Vec<u32> = vec![0; gb::screen_width * gb::screen_height];
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        gb::screen_width,
+        gb::screen_height,
+        WindowOptions {
+            borderless: false,
+            title: true,
+            resize: true,
+            scale: Scale::X4,
+            scale_mode: ScaleMode::UpperLeft,
+            topmost: true,
+            transparency: false,
+            none: false,
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0; // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+            .update_with_buffer(&buffer, gb::screen_width, gb::screen_height)
+            .unwrap();
+    }
+}
